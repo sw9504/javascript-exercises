c42b3cf349b3be477e6c6282d7a57473825135b8
sw9504 <sladewilson9504@gmail.com>
2022-07-07 17:55:27 -0300

    Finished Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currYear = 2022;
+    let oldest = {};
+    let years = 0;
 
+    people.forEach(element => {
+        let aux = 0;
+        if(element.hasOwnProperty("yearOfDeath")){
+            aux = element['yearOfDeath'] - element['yearOfBirth'];
+        }
+        else
+            aux = currYear - element['yearOfBirth'];
+
+        if (aux > years){
+            oldest = element;
+            years = aux;
+        }
+    });
+
+    return oldest; // It returns an Object!!!
 };
 
 // Do not edit below this line
