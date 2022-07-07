50ee80467be66d20cf8a361ed7a7697c6226e9d0
sw9504 <sladewilson9504@gmail.com>
2022-07-07 17:21:55 -0300

    Finished Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    let fibo = [1,1];
+    num = parseInt(num);
+
+    if(num <= 0) return "OOPS";
+    else if(num === 1 || num === 2) return 1;
+    else {
+        let last = 0;
+        let j = 0;
+
+        for (let i = 3;i<=num;i++) {
+            last = fibo[j] + fibo[++j];
+            fibo.push(last);
+        }
+
+        return last;
+    }
 };
 
 // Do not edit below this line
