d43bd1b5dad276507ae12a91ef7ea2af5c5fafe1
sw9504 <sladewilson9504@gmail.com>
2022-07-07 16:48:33 -0300

    Finished Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let clearStr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"").toLowerCase();
+    let len = clearStr.length;
+    
+    for(let i=0; i< (len-1)/2; i++)
+        if(clearStr[i] !== clearStr[len-1-i]) return false;
 
+    return true;
 };
 
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 // Do not edit below this line
 module.exports = palindromes;
