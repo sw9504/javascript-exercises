637ac4408fec2affde3b44c2438b97fce1986e19
sw9504 <sladewilson9504@gmail.com>
2022-07-07 16:25:09 -0300

    Finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,45 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let acum = 0;
+  for(let i=0; i< arr.length;i++)
+    acum += arr[i];
+  
+  return acum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let acum = 1;
 
+  for(let i=0; i< arr.length;i++)
+    acum = acum * arr[i];
+  
+  return acum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let acum = 1;
+  
+	if(a === 1 || a === 0) return 1;
+
+  else {
+    for(let i=2; i <= a;i++)
+      acum = acum * i;
+    }
+  return acum;
+    
 };
+
 
 // Do not edit below this line
 module.exports = {
